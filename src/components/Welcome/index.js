3e11d702f61909d9fdfabaa979d713e2e05ecff5
vyshu <vyshuvemuri06@gmail.com>
2023-04-14 07:17:43 +0000

    solution

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,16 @@
+// Write your code here
+import {component} from 'react'
+import './index.css'
+
+class Welcome extends component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Welcome</h1>
+        <p>Thank you! Happy Learning</p>
+        <button type="button">subscribe</button>
+      </div>
+    )
+  }
+}
+export default Welcome
